4befdf487d567b63b78ebcfa6420decc27856fb1
Alvarez, Mario <mario.b.alvarez@accenture.com>
2022-12-19 11:10:04 -0600

    Start:creation of base files for Tamalli project

---
+++ tamalli/src/main/java/com/accenture/tamalli/TamalliApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.tamalli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamalliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamalliApplication.class, args);
+	}
+
+}
