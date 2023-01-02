9685c6c42846a2a8ca0599d68838450e298f71e6
Alvarez, Mario <mario.b.alvarez@accenture.com>
2023-01-02 16:38:29 -0600

    Add: New DTO for the shopping cart

---
+++ tamalli/src/main/java/com/accenture/tamalli/dto/orders/ShoppingCartDTO.java
@@ -0,0 +1,29 @@
+package com.accenture.tamalli.dto.orders;
+
+import com.accenture.tamalli.dto.orderDetails.OrderDetailDTO;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class ShoppingCartDTO implements Serializable {
+
+    private BigDecimal totalCost;
+    private List<OrderDetailDTO> shoppingCartList;
+
+    public BigDecimal getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(BigDecimal totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public List<OrderDetailDTO> getShoppingCartList() {
+        return shoppingCartList;
+    }
+
+    public void setShoppingCartList(List<OrderDetailDTO> shoppingCartList) {
+        this.shoppingCartList = shoppingCartList;
+    }
+}
