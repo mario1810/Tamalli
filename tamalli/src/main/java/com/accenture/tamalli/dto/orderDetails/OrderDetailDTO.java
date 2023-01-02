b9b52f817a93058c3aa17c6f7dd3202f50e50599
Alvarez, Mario <mario.b.alvarez@accenture.com>
2023-01-02 13:12:13 -0600

    Create: DTOs for order service layer to dilter information

---
+++ tamalli/src/main/java/com/accenture/tamalli/dto/orderDetails/OrderDetailDTO.java
@@ -0,0 +1,53 @@
+package com.accenture.tamalli.dto.orderDetails;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class OrderDetailDTO implements Serializable {
+
+    private Long detailOrderId;
+    private Integer quantityOrdered;
+    private String  productOrdered;
+    private BigDecimal productPriceOrdered;
+    private String  productLine;
+
+    public Long getDetailOrderId() {
+        return detailOrderId;
+    }
+
+    public void setDetailOrderId(Long detailOrderId) {
+        this.detailOrderId = detailOrderId;
+    }
+
+    public Integer getQuantityOrdered() {
+        return quantityOrdered;
+    }
+
+    public void setQuantityOrdered(Integer quantityOrdered) {
+        this.quantityOrdered = quantityOrdered;
+    }
+
+    public String getProductOrdered() {
+        return productOrdered;
+    }
+
+    public void setProductOrdered(String productOrdered) {
+        this.productOrdered = productOrdered;
+    }
+
+    public BigDecimal getProductPriceOrdered() {
+        return productPriceOrdered;
+    }
+
+    public void setProductPriceOrdered(BigDecimal productPriceOrdered) {
+        this.productPriceOrdered = productPriceOrdered;
+    }
+
+    public String getProductLine() {
+        return productLine;
+    }
+
+    public void setProductLine(String productLine) {
+        this.productLine = productLine;
+    }
+}
