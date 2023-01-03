5235d13950089add2488de381e932b3c2685166d
Alvarez, Mario <mario.b.alvarez@accenture.com>
2023-01-02 22:15:53 -0600

    chore: add findByProductId to the product Repository

--- tamalli/src/main/java/com/accenture/tamalli/repositories/IProductRepository.java
+++ tamalli/src/main/java/com/accenture/tamalli/repositories/IProductRepository.java
@@ -4,6 +4,9 @@ import com.accenture.tamalli.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IProductRepository extends JpaRepository<Product,Long> {
+    Optional<Product> findByProductId(Long productId);
 }
