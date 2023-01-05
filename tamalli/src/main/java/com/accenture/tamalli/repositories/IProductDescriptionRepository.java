8fe43a4a215dfb286b2df3b002a18ce181412197
Alvarez, Mario <mario.b.alvarez@accenture.com>
2023-01-04 22:06:21 -0600

    Chore: Create ProductDescription  document and repository

---
+++ tamalli/src/main/java/com/accenture/tamalli/repositories/IProductDescriptionRepository.java
@@ -0,0 +1,14 @@
+package com.accenture.tamalli.repositories;
+
+
+import com.accenture.tamalli.models.ProductDescription;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface IProductDescriptionRepository extends MongoRepository<ProductDescription,Long> {
+
+    Optional<ProductDescription> findByProductId(Long productId);
+
+
+}
