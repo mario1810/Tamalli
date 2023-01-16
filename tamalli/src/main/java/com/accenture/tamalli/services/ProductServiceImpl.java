ae556609f2567e29f4a5b47e7b6976b06cb6888f
Alvarez, Mario <mario.b.alvarez@accenture.com>
2023-01-15 21:06:57 -0600

    Change validation conditions

--- tamalli/src/main/java/com/accenture/tamalli/services/ProductServiceImpl.java
+++ tamalli/src/main/java/com/accenture/tamalli/services/ProductServiceImpl.java
@@ -33,7 +33,7 @@ public class ProductServiceImpl implements IProductService {
     IProductDescriptionRepository iProductDescriptionRepository;
     @Override
     public Drink addDrink(Drink drink) throws RuntimeException {
-        if(drink.equals(null) || drink.getProductName()==null || drink.getCapacityLiters()<=0.0 || drink.getPrice()==null || drink.getPrice().compareTo(new BigDecimal("0.0"))<1)
+        if(drink==null || drink.getProductName()==null || drink.getCapacityLiters()<=0.0 || drink.getPrice()==null || drink.getPrice().compareTo(new BigDecimal("0.0"))<1)
             throw  new BadRequestProductException("please, register a valid product");
         if(!iDrinkRepository.findByProductNameIgnoreCaseAndCapacityLiters(drink.getProductName(),drink.getCapacityLiters()).isEmpty())
             throw  new BadRequestProductException("This product is already in the database");
@@ -43,7 +43,7 @@ public class ProductServiceImpl implements IProductService {
 
     @Override
     public Tamal addTamal(Tamal tamal) throws RuntimeException{
-        if(tamal.equals(null) || tamal.getProductName()==null || tamal.getWeightKilogram()<=0.0 || tamal.getPrice()==null  || tamal.getPrice().compareTo(new BigDecimal("0.0"))<1)
+        if(tamal==null || tamal.getProductName()==null || tamal.getWeightKilogram()<=0.0 || tamal.getPrice()==null  || tamal.getPrice().compareTo(new BigDecimal("0.0"))<1)
             throw  new BadRequestProductException("please, register a valid product");
         if(!iTamalRepository.findByProductNameIgnoreCaseAndWeightKilogram(tamal.getProductName(),tamal.getWeightKilogram()).isEmpty())
             throw  new BadRequestProductException("This product is already in the database");
@@ -89,7 +89,7 @@ public class ProductServiceImpl implements IProductService {
 
     @Override
     public Product changeProductPrice(ProductPriceDTO changesProduct) throws RuntimeException{
-        if(changesProduct.equals(null) || changesProduct.getPrice().compareTo(new BigDecimal("0.0"))<1)
+        if(changesProduct==null || changesProduct.getPrice().compareTo(new BigDecimal("0.0"))<1)
             throw  new BadRequestProductException("please, register valid changes");
         Product productToUpdate =iProductRepository.findByProductId(changesProduct.getProductId()).orElseThrow(()->new NotFoundProductException("There is no product with id:"+changesProduct.getProductId()));
         //Changes price
